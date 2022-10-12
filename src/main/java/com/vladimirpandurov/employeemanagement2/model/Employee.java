47bf5818dee9b4e613428c86805941dd4e9e4dc0
panvla <pandvlado@gmail.com>
2022-10-12 10:09:09 +0200

    added Employee entity. closes #1.

---
+++ src/main/java/com/vladimirpandurov/employeemanagement2/model/Employee.java
@@ -0,0 +1,29 @@
+package com.vladimirpandurov.employeemanagement2.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import sun.util.resources.Bundles;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Employee implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, updatable = false)
+    private Long id;
+    private String name;
+    private String email;
+    private String jobTitle;
+    private String phone;
+    private String imageUrl;
+    @Column(nullable = false, updatable = false)
+    private String employeeCode;
+}
