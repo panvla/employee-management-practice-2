770e2fee56d51d6ce048315ddbc2d1475a66a7d8
panvla <pandvlado@gmail.com>
2022-10-17 21:17:10 +0200

    Added a lot of stuff

--- src/main/java/com/vladimirpandurov/employeemanagement2/EmployeeManagement2Application.java
+++ src/main/java/com/vladimirpandurov/employeemanagement2/EmployeeManagement2Application.java
@@ -2,12 +2,33 @@ package com.vladimirpandurov.employeemanagement2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class EmployeeManagement2Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeManagement2Application.class, args);
+	}
+
+
+	@Bean
+	public CorsFilter corsFilter() {
+		//from stackoverflow
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.addAllowedOrigin("http://localhost:4200");
+		configuration.setAllowedOrigins(Arrays.asList("*"));
+		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+		configuration.setAllowedHeaders(Arrays.asList("authorization", "content-type", "x-auth-token"));
+		configuration.setExposedHeaders(Arrays.asList("x-auth-token"));
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return new CorsFilter(source);
 	}
 
 }
